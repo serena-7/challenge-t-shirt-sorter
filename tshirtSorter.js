4ef8f3070bed3b93e9a21b199e0c585704caeab8
Serena Sorensen <serena.sorensen7@gmail.com>
2022-04-14 10:01:00 -0600

    chall complete

--- tshirtSorter.js
+++ tshirtSorter.js
@@ -1,1 +1,19 @@
 // Write your solution below:
+function tShirtSorter(str){
+    let small = 0;
+    let med = 0;
+    let large = 0;
+  
+    for(let i = 0; i < str.length; i++){
+      if (str[i] === 's'){
+        small++;
+      }else if (str[i] === 'm'){
+        med++;
+      }else if (str[i] === 'l'){
+        large++
+      }
+    }
+    return 's'.repeat(small) + 'm'.repeat(med) + 'l'.repeat(large);
+  }
+  
+  console.log(tShirtSorter('mlssllmls'))
